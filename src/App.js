62c4443d887bbb0f6b0dcf8923f572a4d2f35663
Muhammad Adeel <itsadeel312@gmail.com>
2026-02-16 13:43:07 +0500

    Auto-remove injected malicious payload & vscode infection

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import { Box } from '@mui/system';
-import Textfields from './Components/Base/textfields/index'
-import Buttons from './Components/Base/buttons';
-
-function App() {
-  return (
-    
-    <Box>
-    <Buttons/>
-    <Textfields/>
-    
-    </Box>
-  );
-}
-
+import { Box } from '@mui/system';
+import Textfields from './Components/Base/textfields/index'
+import Buttons from './Components/Base/buttons';
+function App() {
+  return (
+    
+    <Box>
+    <Buttons/>
+    <Textfields/>
+    
+    </Box>
+  );
+}
+
 export default App;
